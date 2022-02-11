da5137c753eb4cd4413ab603e32587a6d2faea8f
bb <46041591+brishti9301@users.noreply.github.com>
2022-02-11 19:12:37 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,83 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What is the rarest M&M color?",
+    answer: "Brown",
+    options: [
+      "Brown",
+      "Blue",
+      "Red",
+      "Yellow"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does Fissures, vents, and plugs are all associated with which geological feature?",
+    answer: "Volcanos",
+    options: [
+      "Fjord",
+      "Volcanos",
+      "Canyons",
+      "Glacier"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What is the tiny piece at the end of a shoelace called?",
+    answer: "Aglet",
+    options: [
+      "Aglet",
+      "Tip",
+      "Cord's end",
+      "Needles"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which of Shakespeare's plays is the longest?",
+    answer: "Hamlet",
+    options: [
+      "Romeo and Juliet",
+      "Hamlet",
+      "Much Ado about nothing",
+      "Twelfth Night"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Who wrote the series Percy Jackson & the Olympians",
+    answer: "Rick Riordan",
+    options: [
+      "JK Rowling",
+      "J. R. R. Tolkien",
+      "Enid Blyton",
+      "Rick Riordan"
+    ]
+  },
+  {
+    numb: 6,
+    question: "Which band wrote and sung Hotel California",
+    answer: "Eagles",
+    options: [
+      "Beatles",
+      "Arctic Monkeys",
+      "Eagles",
+      "One direction"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
